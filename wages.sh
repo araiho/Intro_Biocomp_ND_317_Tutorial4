34ff3d331ebe43914c812c2ec84786d5700fb7ed
Kate Vendrely <kvendrel@nd.edu>
2017-09-15 11:36:37 -0400

    giving clearer usage comment for script

--- wages.sh
+++ wages.sh
@@ -1,5 +1,6 @@
-#gender in first column and yearsExperience in second column with a space
-#sep columns. Rows sorted by gneder and then yearsExperience
+#script takes gender in first column and yearsExperience in second column with a space
+#separating columns. Rows sorted by gneder and then yearsExperience from wages.csv file
+##usage: bash wages.sh and then type cat genderYearExperience.txt
 
 cat wages.csv | awk '$1~/female/{print}' | sort -k2 -n > genderYearExperience.txt
 cat wages.csv | awk '$1~/male/{print}' | sort -k2 -n >> genderYearExperience.txt
