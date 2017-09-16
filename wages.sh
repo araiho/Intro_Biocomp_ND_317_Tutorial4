9693af7e2162c5bfff14581516a0febccfe28b0c
Kate Vendrely <kvendrel@nd.edu>
2017-09-16 12:12:57 -0400

    update to how the file is sorted

--- wages.sh
+++ wages.sh
@@ -1,7 +1,6 @@
 #script takes gender in first column and yearsExperience in second column with a space
-#separating columns. Rows sorted by gneder and then yearsExperience from wages.csv file
+#separating columns. Rows sorted by gender and then yearsExperience from wages.csv file
 ##usage: bash wages.sh and then type cat genderYearExperience.txt
 
-cat wages.csv | awk '$1~/female/{print}' | sort -k2 -n > genderYearExperience.txt
-cat wages.csv | awk '$1~/male/{print}' | sort -k2 -n >> genderYearExperience.txt
-
+cat wages.csv | awk '$1~/female/{print}' | sort -n -k2 | tr "," " " | awk '{print $1 " " $2}' | uniq | sort -n -k2 > genderYearExperience.txt
+cat wages.csv | awk '$1~/male/{print}' | sort -n -k2 | tr "," " " | awk '{print $1 " " $2}' | uniq | sort -n -k2 >> genderYearExperience.txt
