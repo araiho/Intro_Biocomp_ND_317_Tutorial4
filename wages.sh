c605f9343c73efcf3d692a2175a9304199f41fb3
Logan <larnold3@nd.edu>
2017-09-17 20:50:56 -0400

    An attempt at number 3

--- wages.sh
+++ wages.sh
@@ -1,3 +1,5 @@
+#Run with bash wages.sh wages.csv
+
 #shell script to sort through wages.csv
 cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g'
 
@@ -5,9 +7,12 @@ cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g'
 echo Highest Earner, with years of experience:
 cat "$1" | sed 's/,/ /g' | sort -n -k 4 | cut -f 1-2,4 -d " " | tail -1
 echo Lowest Earner, with years of experience:
-cat "$1" | sed 's/,/ /g' | sort -n -k 4 | cut -f 1-2,4 -d " " | head -2 | tail $
+cat "$1" | sed 's/,/ /g' | sort -n -k 4 | cut -f 1-2,4 -d " " | head -2 | tail -1
 echo Women in top 10:
 cat "$1" | sed 's/,/ /g' | sort -n -k 4 | tail -10 | grep -c "female"
 
-
-
+#Shell Script for Number 3
+VAL1=$(cat "$1" | sed 's/,/ /g' | sort -n -k 3,3 -k 4,4 | cut -f 3-4 -d " " | grep "16 " | head -1 | cut -f 2 -d " ")
+VAL2=$(cat "$1" | sed 's/,/ /g' | sort -n -k 3,3 -k 4,4 | cut -f 3-4 -d " " | grep "12 " | head -1 | cut -f 2 -d " ")
+echo Difference in minimum wage between high school and college graduate
+echo "$VAL1 - $VAL2" | bc
