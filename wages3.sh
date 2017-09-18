5182e222d2a60fc9e056c4b01fadf1f054587736
Kate Vendrely <kvendrel@nd.edu>
2017-09-17 20:38:33 -0400

    script now outputs difference in min wage between college and HS grads

--- wages3.sh
+++ wages3.sh
@@ -1,5 +1,12 @@
+#prints the difference of graduating college on average minimum wage for earners in the wages.csv dataset.
+#Final printout is the average minimum wage dollar difference between workers who graduated from college
+#minus the average minimum  wage of workers who graduated from highschool
+##usage: bash wages3.sh
+
+#creates sorted list of minumum wages of college graduates from lowest minimum wage to highest
 cat wages.csv | tr "," " " | awk '{print $3 " " $4}' | awk '$1~/16/{print}' | sort -n > collegeGrads.txt
 
+#creates sorted list of minimum wages of high school graduates from lowest minimum wage to highest
 cat wages.csv | tr "," " " | awk '{print $3 " " $4}' | awk '$1~/12/{print}' | sort -n >> highschoolGrads.txt
 
 #to find average wage of a college grad
@@ -9,4 +16,6 @@ cat collegeGrads.txt | awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }
 cat highschoolGrads.txt | awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }' > "val2"
 
 #to find difference in average minimum wage between a high school and college graduate
-
+avgCollege=7.53786
+avgHS=5.66835
+echo "$avgCollege - $avgHS" | bc
