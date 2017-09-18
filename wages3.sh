2a3cf02e6c9c4f18433c1b0c2537c7c874b84773
Kate Vendrely <kvendrel@nd.edu>
2017-09-17 20:00:41 -0400

    script that calculates average min wage for challenge 3

--- wages3.sh
+++ wages3.sh
@@ -1,1 +1,12 @@
-awk '{print $NF}' wages.csv
+cat wages.csv | tr "," " " | awk '{print $3 " " $4}' | awk '$1~/16/{print}' | sort -n > collegeGrads.txt
+
+cat wages.csv | tr "," " " | awk '{print $3 " " $4}' | awk '$1~/12/{print}' | sort -n >> highschoolGrads.txt
+
+#to find average wage of a college grad
+cat collegeGrads.txt | awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }' > "val1"
+
+#to find average wage of a high school grad
+cat highschoolGrads.txt | awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }' > "val2"
+
+#to find difference in average minimum wage between a high school and college graduate
+
