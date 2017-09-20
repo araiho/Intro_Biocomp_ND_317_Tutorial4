dc36f2a2aada6c39fd859550cbbc56bcda40f2cb
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2017-09-20 10:52:42 -0400

    Saving edits to install bc

--- wageanalysis.sh
+++ wageanalysis.sh
@@ -3,3 +3,7 @@
 cat wages.csv|sed 's/,/ /g'|sort -k 4 -n|tail -n 1
 cat wages.csv|sed 's/,/ /g'|sort -k 4 -n|head -n 2|tail -n 1
 cat wages.csv|sed 's/,/ /g'|sort -k 4 -n|tail -n 10| grep female| wc -l
+
+val1=$(cat wages.csv|sed 's/,/ /g'|sort -k 3 -n|tail -n 16|sed 's/ /,/g'|cut -d , -f 4|sort -n|head -n 1)
+val2=$(cat wages.csv|sed 's/,/ /g'|sort -k 3 -n|head -n 2542|tail -n 1188|sed 's/ /,/g'|cut -d , -f 4|sort -n|head -n 1)
+echo val1 - val2| bc
