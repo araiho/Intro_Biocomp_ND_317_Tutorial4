d428b01e783e018eeb74d32e43267682d9557fc6
Bhavana Palakurthi <bpalakur@nd.edu>
2017-09-21 02:35:39 +0100

    assign values for val2

--- wageanalysis.sh
+++ wageanalysis.sh
@@ -3,3 +3,4 @@
 cat wages.csv|sed 's/,/ /g'|sort -k 4 -n|tail -n 1
 cat wages.csv|sed 's/,/ /g'|sort -k 4 -n|head -n 2|tail -n 1
 cat wages.csv|sed 's/,/ /g'|sort -k 4 -n|tail -n 10| grep female| wc -l
+val2=$(sed 's/,/ /g' wages.csv | sort -k 3 -n | head -n 2542 | tail -n 1187 | sort -k 4 -n | head -n 1| sed 's/ /,/g' | cut -d , -f 4)
