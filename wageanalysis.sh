2dab19d2fdcd041660d61505e426b07281b56b9a
Bhavana Palakurthi <bpalakur@nd.edu>
2017-09-21 02:41:12 +0100

    subtract val2 from val1

--- wageanalysis.sh
+++ wageanalysis.sh
@@ -5,3 +5,4 @@ cat wages.csv|sed 's/,/ /g'|sort -k 4 -n|head -n 2|tail -n 1
 cat wages.csv|sed 's/,/ /g'|sort -k 4 -n|tail -n 10| grep female| wc -l
 val2=$(sed 's/,/ /g' wages.csv | sort -k 3 -n | head -n 2542 | tail -n 1187 | sort -k 4 -n | head -n 1| sed 's/ /,/g' | cut -d , -f 4)
 val1=$(sed 's/,/ /g' wages.csv | sort -k 3 -n | tail -n 15 | sed 's/ /,/g' | cut -d , -f 4 | sort -n | head -n 1)
+echo "val1-val2" | bc
